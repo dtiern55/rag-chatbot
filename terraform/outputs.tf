14261bc4fe13df136b79d713868f93ffabb5a08b
Danny <dannyjtierney@gmail.com>
2025-11-26 09:48:49 -0600

    outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,37 @@
+# Lambda ARNs
+output "text_extractor_arn" {
+  description = "ARN of the text extractor Lambda"
+  value       = aws_lambda_function.text_extractor.arn
+}
+
+output "text_chunker_arn" {
+  description = "ARN of the text chunker Lambda"
+  value       = aws_lambda_function.text_chunker.arn
+}
+
+output "embedding_generator_arn" {
+  description = "ARN of the embedding generator Lambda"
+  value       = aws_lambda_function.embedding_generator.arn
+}
+
+output "data_storer_arn" {
+  description = "ARN of the data storer Lambda"
+  value       = aws_lambda_function.data_storer.arn
+}
+
+# DynamoDB
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB metadata table"
+  value       = aws_dynamodb_table.metadata.name
+}
+
+# S3 Buckets
+output "input_bucket_name" {
+  description = "Name of the input S3 bucket"
+  value       = aws_s3_bucket.input.bucket
+}
+
+output "output_bucket_name" {
+  description = "Name of the output S3 bucket"
+  value       = aws_s3_bucket.output.bucket
+}
