53a670b6ca9eca5ff365603d4c58fea6a72bd757
Danny <dannyjtierney@gmail.com>
2025-11-26 09:57:29 -0600

    remove outputs from dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -40,14 +40,3 @@ resource "aws_dynamodb_table" "metadata" {
     ManagedBy   = "Terraform"
   }
 }
-
-# Output the table name for use in other configs
-output "dynamodb_table_name" {
-  description = "Name of the DynamoDB metadata table"
-  value       = aws_dynamodb_table.metadata.name
-}
-
-output "dynamodb_table_arn" {
-  description = "ARN of the DynamoDB metadata table"
-  value       = aws_dynamodb_table.metadata.arn
-}
